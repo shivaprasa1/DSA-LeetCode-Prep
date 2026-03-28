fc2710525e91c8bc8206c66510524e29ce750f9d
Shiva Prasad R <142129928+shivaprasa1@users.noreply.github.com>
2026-03-28 17:11:42 +0530

    Update 7_Reverse_Integer.py

--- Day-1/7_Reverse_Integer.py
+++ Day-1/7_Reverse_Integer.py
@@ -4,6 +4,9 @@
 # Extract digits using %10
 # Build reverse using rev*10 + digit
 
+
+#approch - 01
+
 class Solution:
     def reverse(self, x: int) -> int:
         sign = -1 if x < 0 else 1
@@ -16,3 +19,14 @@ class Solution:
             x //= 10
 
         return sign * rev 
+
+
+
+
+#approch - 02
+
+sign = -1 if x < 0 else 1
+x=abs(x)
+ans=sign*int(str(x)[::-1]))
+return ans
+
