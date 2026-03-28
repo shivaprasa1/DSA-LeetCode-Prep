16b7f84c5b1b8a27724452cf19fedb46b7f5a55a
Shiva Prasad R <142129928+shivaprasa1@users.noreply.github.com>
2026-03-28 17:22:12 +0530

    Create 258_Add_Digits.py

---
+++ Day-2/258_Add_Digits.py
@@ -0,0 +1,19 @@
+# Problem: 258 - Add Digits
+
+#logic :
+#Extract digit and add each digit 
+#take that result number
+#again add each digit of that number
+#Reapet Until number become single digit
+
+class Solution:
+    def addDigits(self, num: int) -> int:
+      
+        while num >= 10:
+            total = 0
+            while num > 0:
+                digit = num % 10
+                total += digit
+                num //= 10
+            num = total
+        return num
