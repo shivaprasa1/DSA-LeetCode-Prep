6e04b5598e448550eb29d3eb98fbc124a95c956f
Shiva Prasad R <142129928+shivaprasa1@users.noreply.github.com>
2026-03-28 17:37:12 +0530

    Create 2520_Count_Digits.py

---
+++ Day-3/2520_Count_Digits.py
@@ -0,0 +1,16 @@
+# Problem: 2520 - Count Digits
+#logic:
+#Extract each digit
+#check if that digit is dividion by that org number
+#if it is divided then count each number of that 
+
+class Solution:
+    def countDigits(self, num: int) -> int:
+        temp = num
+        count = 0
+        while temp > 0:
+            digit = temp % 10
+            if digit != 0 and num % digit == 0:
+                count += 1
+            temp //= 10
+        return count
