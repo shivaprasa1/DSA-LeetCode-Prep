d70dd0e3e39cddcf90ac72f5fa5c3221b495b9ad
Shiva Prasad R <142129928+shivaprasa1@users.noreply.github.com>
2026-03-28 17:24:39 +0530

    Create 1281_Product_Sum.py

---
+++ Day-2/1281_Product_Sum.py
@@ -0,0 +1,19 @@
+# Problem: 1281 - Subtract Product and Sum
+
+#logic :
+#Extract digit
+#Do two opertion 
+#1) sum+=digit
+#2) product*=digit
+
+class Solution:
+    def subtractProductAndSum(self, n: int) -> int:
+        sum_val = 0
+        product = 1
+        while n > 0:
+            digit = n % 10
+            sum_val += digit
+            product *= digit
+            n //= 10
+
+        return product - sum_val
