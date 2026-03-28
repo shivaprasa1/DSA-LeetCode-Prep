a8da7af06bd94126af51421556ac89bddbf62038
Shiva Prasad R <142129928+shivaprasa1@users.noreply.github.com>
2026-03-28 17:39:43 +0530

    Create 1342_Reduce_to_Zero.py

---
+++ Day-3/1342_Reduce_to_Zero.py
@@ -0,0 +1,13 @@
+# Problem: 1342 - Reduce to Zero
+
+class Solution:
+    def numberOfSteps(self, num: int) -> int:
+        steps = 0
+
+        while num > 0:
+            if num % 2 == 0:
+                num //= 2
+            else:
+                num -= 1
+            steps += 1
+        return steps
